7c4cdafa3db1fc0e6e2204da55be126543b1d304
Peter <pomochieng@gmail.com>
2018-11-03 19:38:52 +0300

    Add object details and files

---
+++ objects/object.js
@@ -0,0 +1,35 @@
+new Vue({
+    el: '#header',
+    data: {
+          message: 'KIDS KATA'
+
+    }
+
+})
+
+new Vue({
+    el: '#object',
+    data: {
+          message: 'Learn All the Objects in the world Here.'
+
+    }
+
+})
+
+new Vue({
+    el: '#query',
+    data: {
+          message: 'What is the name of this picture?'
+
+    }
+
+})
+
+new Vue({
+    el: '#answer',
+    data: {
+          message: 'The Answer is....'
+
+    }
+
+})

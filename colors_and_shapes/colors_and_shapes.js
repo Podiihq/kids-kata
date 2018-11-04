f131b863230f2abf6b4ea35c20a410a95f2aff31
7_5P <76pollet@gmail.com>
2018-11-04 18:33:56 +0300

    logged the result of the image passed through the api on the console

--- colors_and_shapes/colors_and_shapes.js
+++ colors_and_shapes/colors_and_shapes.js
@@ -17,14 +17,34 @@ new Vue ({
 	data: {
 		no1: 'choice one' ,
 		no2: 'choice two',
-		no3: 'choice three'
+		no3: 'choice three',
+          }
+    
 
-	}
-})
+});
+
+function answer(){
+const app = new Clarifai.App({
+				apiKey: '215329a0d20941a7815b809a6650d0f2'
+
+				})
+
+			app.models.predict(Clarifai.COLOR_MODEL, 'http://www.acdivoca.org.co/wp-content/uploads/bfi_thumb/dummy-4-n6weeqyjuuv1p400l2c69lnocx5uo9njt251d2zrsg.png').then(
+				function(response){
+					console.log(response.outputs[0].data.colors[0].w3c.name)
+
+				},
+				function(err){
+					console.log(err)
+				})
+}
+
 var displayans = new Vue ({
 	el: '#displayans',
 	data: {
 		collapsed: true,
-		ans: 'The answer is'
+		ans: 'the answer is '
 	}
 });
+
+ 

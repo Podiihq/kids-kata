3f6b54c1f2767fa5a050bacb9b0f30a60d4d49dd
Peter <pomochieng@gmail.com>
2018-11-04 18:24:09 +0300

    Syntax errors

--- objects/object.js
+++ objects/object.js
@@ -1,49 +1,61 @@
 new Vue({
-    el: '#header',
-    data: {
-          message: 'KIDS KATA'
+  el: '#header',
+  data: {
+    message: 'KIDS KATA'
 
-    }
-
-})
-
-new Vue({
-    el: '#object',
-    data: {
-          message: 'Learn All the Objects in the world Here.'
-
-    }
+  }
 
 })
 
 new Vue({
-    el: '#query',
-    data: {
-          message: 'What is the name of this picture?'
+  el: '#object',
+  data: {
+    message: 'Learn All the Objects in the world Here.'
 
-    }
+  }
 
 })
 
 new Vue({
-    el: '#answer',
-    data: {
-          message: 'The Answer is....'
+  el: '#query',
+  data: {
+    message: 'What is the name of this picture?'
 
-    }
+  }
 
 })
 
-  const app = new Clarifai.App({
-    apiKey: '817dc940f3764c3b91d389b1bc982d10'
-  });
+new Vue({
+  el: '#answer',
+  data: {
+    message: 'The Answer is....'
 
-  app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
-    function(response) {
-      console.log(response.outputs[0].data.concepts[0].name);
-    },
-    function(err) {
-      console.error(err);
+  }
+
+})
+
+new Vue({
+  data() {
+    return {
+      FetchedRestuls: []
+
     }
-  );
+
+  }
+
+})
+
+
+const app = new Clarifai.App({
+  apiKey: '817dc940f3764c3b91d389b1bc982d10'
+});
+
+app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+  function(response) {
+    console.log(response.outputs[0].data.concepts[0].name);
+  },
+  function(err) {
+    console.error(err);
+  }
+);
 
